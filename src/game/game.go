8aa0354015bf434c2f7ae024188c09685db84a4c
Marcelo Junior <marcelo.jr63@gmail.com>
2020-09-22 20:34:40 -0300

    Update method init of package game

--- src/game/game.go
+++ src/game/game.go
@@ -2,20 +2,36 @@ package game
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/MarceloMPJ/contragirl/src/keyboard"
 	keyboardlib "github.com/eiannone/keyboard"
 )
 
+var playerone player
+
+type player struct {
+	name string
+	hp   int
+	posx int
+	posy int
+}
+
 func init() {
 	fmt.Println("Carregando recursos ...")
+	playerone = player{name: "Joaquim", hp: 100, posx: 0, posy: 0}
 	keyboard.AddObserver(ProcessComand)
+	go keyboard.ListenKeyboard()
 }
 
 // Start é a função usada para iniciar o jogo
 func Start() {
 	fmt.Println("Iniciando o Jogo")
-	keyboard.ListenKeyboard()
+
+	for {
+		time.Sleep(10 * time.Second)
+		fmt.Println(playerone)
+	}
 	fmt.Println("Finalizando o Jogo")
 }
 
